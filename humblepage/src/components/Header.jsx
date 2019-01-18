5946021aeb0771e3c8e773be962ce01f9d58d9e9
John cClaro <jkrclaro@gmail.com>
2019-01-18 23:58:49 +0000

    New logo

--- humblepage/src/components/Header.jsx
+++ humblepage/src/components/Header.jsx
@@ -5,17 +5,17 @@ const logo = require('../imgs/logo.png');
 const styles = {
     border: {backgroundColor: '#7289DA', color: '#111', borderRadius: '10%'},
     font: {fontFamily: 'Times New Roman', fontWeight: 700},
-    brand: {color: '#111', fontSize: 42, fontWeight: 700, textDecoration: 'none'}
+    brand: {color: '#111', fontSize: 40, fontWeight: 700, textDecoration: 'none'}
 }
 
 class Header extends React.Component {
 
     render() {
         return (
-            <ul className="nav justify-content-center mb-3">
+            <ul className="nav justify-content-center mb-5">
                 <li className="nav-item">
                     <a className='nav-link' style={styles.brand} href="/">
-                        <img src={logo} alt='logo' className='img-fluid' height='50' width='50'/> humblepage
+                        <img src={logo} alt='logo' className='img-fluid' height='60' width='60'/> humblepage
                     </a>
                 </li>
             </ul>
