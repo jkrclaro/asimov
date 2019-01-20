ed39ecbe832558adf59c9d63e78bfaae3f64b7ab
John cClaro <jkrclaro@gmail.com>
2019-01-20 13:53:26 +0000

    Go back button

--- humblepage/src/components/Contact.jsx
+++ humblepage/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Contact extends React.Component {
@@ -8,11 +9,12 @@ class Contact extends React.Component {
             <div>
                 <div className='container'>
                     <h4><b>Get in touch via form</b></h4>
-                    <form action='https://formspree.io/gethumblepage@gmail.com' method='POST'>
+                    <form action='https://formspree.io/gethumblepage@gmail.com' method='POST' className='mb-3'>
                         <input type='email' name='email' placeholder='Your email' className='form-control mb-3'></input>
                         <textarea name='message' placeholder='Your message' className='form-control mb-3'></textarea>
                         <input type='submit' className='btn btn-humblepage-alternative btn-block' value='Send message'></input>
                     </form>
+                    <Link to='/' className='btn btn-humblepage-primary'>Go back</Link>
                 </div>
             </div>
         )
