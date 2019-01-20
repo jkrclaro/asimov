1a96e0328b737e2748d879c10fee57df041fa59d
John cClaro <jkrclaro@gmail.com>
2019-01-20 14:26:54 +0000

    Go back button

--- humblepage/src/components/Contact.jsx
+++ humblepage/src/components/Contact.jsx
@@ -9,12 +9,12 @@ class Contact extends React.Component {
         return (
             <div>
                 <div className='container'>
-                    <Link to='/' className='btn btn-humblepage-primary mb-3'><FontAwesomeIcon icon='chevron-left'/> Go back</Link>
+                    <Link to='/' className='btn btn-humblepage-alternative mb-3'><FontAwesomeIcon icon='chevron-left'/> Go back</Link>
                     <h4><b>Get in touch via form</b></h4>
                     <form action='https://formspree.io/gethumblepage@gmail.com' method='POST'>
                         <input type='email' name='email' placeholder='Your email' className='form-control mb-3'></input>
                         <textarea name='message' placeholder='Your message' className='form-control mb-3'></textarea>
-                        <input type='submit' className='btn btn-humblepage-alternative btn-block' value='Send message'></input>
+                        <input type='submit' className='btn btn-humblepage-primary btn-block' value='Send message'></input>
                     </form>
                 </div>
             </div>
