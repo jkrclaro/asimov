ecce430d7211165807ff57ad7210ddf3d2214ed8
John cClaro <jkrclaro@gmail.com>
2019-01-30 20:25:49 +0000

    Remove anchor on footer

--- humblepage/src/components/Footer.jsx
+++ humblepage/src/components/Footer.jsx
@@ -14,10 +14,10 @@ class Footer extends React.Component {
             <div>
                 <ul className="nav justify-content-center mt-5 mb-3">
                     <li className="nav-item text-center">
-                        <a className='nav-link disabled' href='/'>
+                        <span className='nav-link disabled'>
                             <img src={logo} className='img-fluid mr-3' height='50' width='50'></img>
                             <b>Made with <FontAwesomeIcon icon='heart' style={styles.heart}/> in Dublin, Ireland</b>
-                        </a>
+                        </span>
                     </li>
                 </ul>
             </div>
