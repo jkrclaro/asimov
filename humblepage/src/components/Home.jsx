65808f87f14fd80f76944ab320476bb23afe44af
John cClaro <jkrclaro@gmail.com>
2019-02-01 20:13:12 +0000

    Just email and message

--- humblepage/src/components/Home.jsx
+++ humblepage/src/components/Home.jsx
@@ -181,13 +181,9 @@ class Home extends React.Component {
                                 </div>
                                 <form action='https://formspree.io/gethumblepage@gmail.com' method='POST'>
                                     <div className='row'>
-                                        <div className='col-lg-6'>
+                                        <div className='col-lg-12'>
                                             <label htmlFor='_replyto'><small>EMAIL <span style={{color: '#E34343'}}>*</span></small></label>
                                             <input type='email' id='_replyto' name='_replyto' className='form-control mb-3' required></input>
-                                        </div>
-                                        <div className='col-lg-6'>
-                                            <label htmlFor='phone'><small>PHONE</small></label>
-                                            <input type='text' id='phone' name='phone' className='form-control mb-3'></input>
                                         </div>
                                         <div className='col-lg-12'>
                                             <label className='message'><small>MESSAGE <span style={{color: '#E34343'}}>*</span></small></label>
