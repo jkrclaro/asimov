d32ce91392f247e5456ac111c049b6a2269c9f53
John cClaro <jkrclaro@gmail.com>
2019-02-05 14:40:04 +0000

    Fix href on footer

--- humblepage/src/components/Footer.jsx
+++ humblepage/src/components/Footer.jsx
@@ -34,30 +34,30 @@ class Footer extends React.Component {
                             </div>
                             <div className='col-lg-3 mt-4'>
                                 <h5 className='text-muted mb-3'><b>EXPLORE</b></h5>
-                                <h5><Link to='/work' className='white-color' href='/'>Work</Link></h5>
-                                <h5><Link to='/services' className='white-color' href='/'>Services</Link></h5>
-                                <h5><Link to='/about' className='white-color' href='/'>About</Link></h5>
-                                <h5><Link to='/contact' className='white-color' href='/'>Contact</Link></h5>
+                                <h5><Link to='/work' className='white-color'>Work</Link></h5>
+                                <h5><Link to='/services' className='white-color'>Services</Link></h5>
+                                <h5><Link to='/about' className='white-color'>About</Link></h5>
+                                <h5><Link to='/contact' className='white-color'>Contact</Link></h5>
                             </div>
                             <div className='col-lg-3 mt-4'>
                                 <h5 className='text-muted mb-3'><b>FOLLOW</b></h5>
-                                <h5><a href='https://www.facebook.com' className='white-color' href='/'>Facebook</a></h5>
-                                <h5><a href='https://www.instagram.com' className='white-color' href='/'>Instagram</a></h5>
-                                <h5><a href='https://www.twitter.com' className='white-color' href='/'>Twitter</a></h5>
-                                <h5><a href='https://www.linkedin.com' className='white-color' href='/'>LinkedIn</a></h5>
+                                <h5><a href='https://www.facebook.com' className='white-color'>Facebook</a></h5>
+                                <h5><a href='https://www.instagram.com' className='white-color'>Instagram</a></h5>
+                                <h5><a href='https://www.twitter.com' className='white-color'>Twitter</a></h5>
+                                <h5><a href='https://www.linkedin.com' className='white-color'>LinkedIn</a></h5>
                             </div>
                             <div className='col-lg-3 mt-4'>
                                 <h5 className='text-muted mb-3'><b>GET IN TOUCH</b></h5>
-                                <h5><a href='mailto:gethumblepage@gmail.com' className='white-color' href='/'>gethumblepage@gmail.com</a></h5>
-                                <h5><a href='tel:1231234567' className='white-color' href='/'>(123) 123-4567</a></h5>
+                                <h5><a href='mailto:gethumblepage@gmail.com' className='white-color'>gethumblepage@gmail.com</a></h5>
+                                <h5><a href='tel:1231234567' className='white-color'>(123) 123-4567</a></h5>
                             </div>
                         </div>
                     ) : (
                         <div className='col-lg-12 text-center'>
                             <h3 className='mb-5'><b>HUMBLEPAGE</b></h3>
                             <h5 className='text-muted mb-3'><b>GET IN TOUCH</b></h5>
-                            <h5><a href='mailto:gethumblepage@gmail.com' className='white-color' href='/'>gethumblepage@gmail.com</a></h5>
-                            <h5><a href='tel:1231234567' className='white-color' href='/'>(123) 123-4567</a></h5>
+                            <h5><a href='mailto:gethumblepage@gmail.com' className='white-color'>gethumblepage@gmail.com</a></h5>
+                            <h5><a href='tel:1231234567' className='white-color'>(123) 123-4567</a></h5>
                         </div>
                     )}
                 </div>
