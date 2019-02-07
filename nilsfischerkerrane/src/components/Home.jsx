5a98a8d2a84c97f8f145215efab96c7dde6829b3
John cClaro <jkrclaro@gmail.com>
2019-02-07 23:12:36 +0000

    Carousel

--- nilsfischerkerrane/src/components/Home.jsx
+++ nilsfischerkerrane/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './Header';
 
+const stock1 = require('../imgs/stock1.jpg');
 const stock2 = require('../imgs/stock2.jpg');
 const stock3 = require('../imgs/stock3.jpg');
 
@@ -40,21 +41,26 @@ class Home extends React.Component {
                         <div className='col-lg-6 mb-5'>
                             <img src={stock2} alt='stock-2' className='img-fluid'></img>
                         </div>
-                        <div className='col-lg-6 mb-5'>
-                            <img src={stock3} alt='stock-3' className='img-fluid'></img>
-                        </div>
-                        <div className='col-lg-6 mb-5'>
+                        <div className='col-lg-12 mb-5'>
                             <h3 className='h3-heading'>WHAT WE'LL DO</h3>
-                            <b>+++++Pint INCLUDED in the price of the tour+++++</b>
                             <p className='p-content'>
                             Join me as we take a journey through Dublin’s and Ireland’s history, focussing on the most important figures of the last centuries; from St. Patrick to Queen Elizabeth, from Oscar Wilde to Bono. 
                             As we make our way through the city, we’ll discuss it all - from the first English invasion up to Brexit, from the arrival of St. Patrick up to the abortion referendum of 2018. And everything in between. We’ll see how this history has shaped Dublin, and how its legacy can still be seen all over the city today. 
                             After 2 hours of history, we'll head to one of my favourite pubs and enjoy a pint of Guinness (pint is included in the price of the tour). As we enjoy it, I’ll tell you everything there is to know about the beer, especially important info that you wouldn't necessarily hear at the Guinness Storehouse – Why does it take so long to pour a pint? Why is it so beloved in Ireland? Why is it so smooth and creamy? Why does it taste so much better here than elsewhere? Why does every can of Guinness have a plastic ball in it?
                             Fascinating history followed by a delicious pint. What could be better?
                             </p>
-                        </div>
+                            <div class="carousel">
+                                <div class="carousel-row">
+                                    <div class="carousel-tile"><a href='/'><img src={stock1} className='img-fluid'></img></a></div>
+                                    <div class="carousel-tile"><a href='/'><img src={stock1} className='img-fluid'></img></a></div>
+                                    <div class="carousel-tile"><a href='/'><img src={stock1} className='img-fluid'></img></a></div>
+                                    <div class="carousel-tile"><a href='/'><img src={stock1} className='img-fluid'></img></a></div>
+                                    <div class="carousel-tile"><a href='/'><img src={stock1} className='img-fluid'></img></a></div>
+                                </div>
+                                </div>
+                            </div>
                         <div className='col-lg-12 mb-5'>
-                            <h3 className='h3-heading'>GALLERY</h3>
+                            <h3 className='h3-heading'>PHOTOS</h3>
                             <div className='row'>
                                 <div className='col-6 col-sm-4 col-lg-2 mb-3'>
                                     <a href='/'><img src='https://via.placeholder.com/150' width='100%'></img></a>
