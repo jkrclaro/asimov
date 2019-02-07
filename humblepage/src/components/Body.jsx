c1a6b2d15f25cc796e10978ab5fad467876cb398
John cClaro <jkrclaro@gmail.com>
2019-02-07 13:35:21 +0000

    NotFound page

--- humblepage/src/components/Body.jsx
+++ humblepage/src/components/Body.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import NotFound from './NotFound';
 
 
 class Body extends React.Component {
 
     render() {
         return (
-            <div>
+            <Switch>
                 <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
-                <Route exact path={process.env.PUBLIC_URL + '/about'} component={About} />
-                <Route exact path={process.env.PUBLIC_URL + '/contact'} component={Contact} />
-            </div>
+                <Route path={process.env.PUBLIC_URL + '/about'} component={About} />
+                <Route path={process.env.PUBLIC_URL + '/contact'} component={Contact} />
+                <Route component={NotFound} />
+            </Switch>
         )
     }
 }
---
+++ humblepage/src/components/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class NotFound extends React.Component {
+
+    render() {
+        return (
+            <div className='container mt-5'>             
+                <h1>404</h1> 
+            </div>
+        )
+    }
+}
+
+export default NotFound;
