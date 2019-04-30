b1bf0853539208f1f12f9be9808c7db3d6f6a758
John cClaro <jkrclaro@gmail.com>
2019-05-01 00:00:43 +0100

    Remove declaration of logging in app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import logging
 from dotenv import find_dotenv, load_dotenv
 
 from webprechaun import create_app
-
-logging.getLogger().setLevel(logging.INFO)
 
 load_dotenv(find_dotenv())
 app = create_app()
