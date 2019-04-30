562bb95de7fd55df3940d33c10fe596a89844033
John cClaro <jkrclaro@gmail.com>
2019-04-30 18:01:36 +0100

    Install psycopg2-binar

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='webprechaun',
+    version='0.1',
+    description='CLI for Webprechaun',
+    url='http://github.com/jkrclaro/webprechaun',
+    author='John Claro',
+    author_email='john@webprechaun.com',
+    zip_safe=False
+)
