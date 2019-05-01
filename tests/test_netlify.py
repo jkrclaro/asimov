7f06bce79a76efc21dce107f8a2f29c7a9060c35
John cClaro <jkrclaro@gmail.com>
2019-05-01 21:02:27 +0100

    Add ability to create site via netlify

--- tests/test_netlify.py
+++ tests/test_netlify.py
@@ -1,14 +1,24 @@
+import os
 import unittest
+from dotenv import find_dotenv, load_dotenv
 
 from webprechaun.libs.netlify import Netlify
+
+
+load_dotenv(find_dotenv())
 
 
 class TestNetlify(unittest.TestCase):
 
     def setUp(self):
-        self.netlify = Netlify('12345')
+        self.netlify = Netlify(os.environ['NETLIFY_ACCESS_TOKEN'])
 
     def test_create_site(self):
+        self.netlify.create_site('webprechaun-1')
+        assert True
+
+    def test_get_sites(self):
+        self.netlify.get_sites()
         assert True
 
 
