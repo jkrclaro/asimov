f5c7203a32ff7d2da2b5061934537f04e19c0dcc
John cClaro <jkrclaro@gmail.com>
2019-06-06 01:34:03 +0100

    Update home page

--- src/pxdcast/models.py
+++ src/pxdcast/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
 
 from .choices import CITIES
 from .managers import ProfileManager
@@ -10,16 +11,13 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    city = models.CharField(max_length=30, choices=CITIES)
-    picture = models.FileField(upload_to='profiles', blank=True, null=True)
-    birthday = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    account = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     objects = ProfileManager()
 
     class Meta:
         db_table = 'profiles'
 
     def __str__(self):
-        return self.account.username
+        return self.user.username
