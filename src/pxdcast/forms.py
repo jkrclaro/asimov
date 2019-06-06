88b839529ca58e793600877b709b605b26ab8db8
John cClaro <jkrclaro@gmail.com>
2019-06-06 09:23:27 +0100

    Stylize signup form

--- src/pxdcast/forms.py
+++ src/pxdcast/forms.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, required=True)
-    last_name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+        fields = ('username', 'email', 'password1', 'password2',)
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.fields['password2'].label = 'Confirm password'
