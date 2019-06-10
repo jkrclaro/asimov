59b8b9b9125cf54cff6fb0f905f172cbf115949d
John cClaro <jkrclaro@gmail.com>
2019-06-10 17:07:26 +0100

    Add footer

---
+++ src/rewardp/models/program.py
@@ -0,0 +1,11 @@
+# from django.db import models
+
+
+# class ProgramManager(models.Manager):
+#     pass
+
+
+# class Program(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     objects = ProgramManager()
