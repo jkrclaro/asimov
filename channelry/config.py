28ad0e5a752ce975b4dda4b608cc192fed23421c
John cClaro <jkrclaro@gmail.com>
2019-06-18 16:49:28 +0100

    Remove hardcoded settings

--- channelry/config.py
+++ channelry/config.py
@@ -5,21 +5,8 @@ media = os.path.join(os.getcwd(), 'media')
 
 
 class Config(object):
-    # flask
-    SECRET_KEY = os.environ['SECRET_KEY']
-
     # sqlalchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
-
-    # flask-mail
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = os.environ['MAIL_USERNAME']
-    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-    MAIL_DEFAULT_SENDER = f"Channelry '{MAIL_USERNAME}'"
 
 
 class Production(Config):
