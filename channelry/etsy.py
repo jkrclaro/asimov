effb4180dbe410456c1981eeac6bf1fcdb8c995c
John cClaro <jkrclaro@gmail.com>
2019-06-18 15:46:06 +0100

    Try with >>>

--- channelry/etsy.py
+++ channelry/etsy.py
@@ -32,8 +32,8 @@ class Etsy:
         """Perform OAuth authentication to get login URL.
 
         Get the login URL by via
-        >> response = self.request_token(['email_r'])
-        >> login_url = response.text
+        >>> response = self.request_token(['email_r'])
+        >>> login_url = response.text
 
         :param scopes: Permission scopes to be enabled.
         """
