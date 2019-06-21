574253d837cfc60b21f44ad09df0eac113599e7a
John cClaro <jkrclaro@gmail.com>
2019-06-21 14:53:45 +0100

    Working test

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,82 +1,73 @@
 import os
-import unittest
-import tempfile
 import json
+import tempfile
 
 import pytest
 
-# unit
-from src.channelry.models.account import User
-
-# integration
-from src.channelry.models import db
 from src.channelry import create_app
-from src.channelry.forms.account import SignupForm
+from src.channelry.models import db
 
 
-app = create_app('testing')
+@pytest.fixture
+def client():
+    app = create_app('testing')
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    client = app.test_client()
+
+    with app.app_context():
+        db.create_all(app=app)
+
+    yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
 
 
-class Unit(unittest.TestCase):
+def test_should_fail_when_signup_form_is_empty(client):
+    response = client.post(
+        '/signup',
+        data=json.dumps({}),
+        content_type='application/json'
+    )
 
-    def setUp(self):
-        self.email = 'john@doe.com'
-        self.password = 'johndoe12345'
-        self.name = 'John Doe'
-
-    def test_password_hash(self):
-        user = User(self.email, self.password, self.name)
-        assert len(user.password) == 60
-
-    def test_password_match(self):
-        user = User(self.email, self.password, self.name)
-        assert user.password_match('johndoe12345')
+    assert response.status_code == 400, response.json
 
 
-class Integration(unittest.TestCase):
+def test_should_pass_when_signup_form_is_valid(client):
+    response = client.post(
+        '/signup',
+        data=json.dumps({
+            'email': 'johndoe@gmail.com',
+            'name': 'John Doe',
+            'password': 'johndoe12345',
+            'confirm': 'johndoe12345'
+        }),
+        content_type='application/json'
+    )
 
-    def setUp(self):
-        self.email = 'john@johndoedomainorigin.com'
-        self.name = 'John Doe'
-        self.password = 'johndoe12345'
-        self.headers = {'content_type': 'application/json'}
-        self.signup_data = {
-            'email': self.email,
-            'name': self.name,
-            'password': self.password,
-            'confirm': self.password
-        }
-        self.app = app
-        self.client = self.app.test_client()
-        with self.app.app_context():
-            db.create_all(app=self.app)
-
-    def test_signup_form_is_empty(self):
-        response = self.client.post('/signup', **self.headers)
-        assert response.status_code == 400, response.json
-
-    def test_signup_form_is_valid(self):
-        data = json.dumps(self.signup_data)
-        response = self.client.post('/signup', data=data, **self.headers)
-        assert response.status_code == 200, response.json
-        assert response.json == {'email': self.email}
-
-    def test_signup_form_email_already_exists(self):
-        data = json.dumps(self.signup_data)
-        self.client.post('/signup', data=data, **self.headers)
-        response = self.client.post('/signup', data=data, **self.headers)
-        assert response.status_code == 409, response.json
-        assert response.json == {'email': 'Email is already taken'}
-
-    def tearDown(self):
-        print('tearing down...')
+    assert response.status_code == 200, response.json
 
 
-class Functional(unittest.TestCase):
+def test_should_fail_when_signup_form_email_exists_already(client):
+    client.post(
+        '/signup',
+        data=json.dumps({
+            'email': 'johndoe@gmail.com',
+            'name': 'John Doe',
+            'password': 'johndoe12345',
+            'confirm': 'johndoe12345'
+        }),
+        content_type='application/json'
+    )
+    response = client.post(
+        '/signup',
+        data=json.dumps({
+            'email': 'johndoe@gmail.com',
+            'name': 'John Doe',
+            'password': 'johndoe12345',
+            'confirm': 'johndoe12345'
+        }),
+        content_type='application/json'
+    )
 
-    def test_signup(self):
-        assert True
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert True
