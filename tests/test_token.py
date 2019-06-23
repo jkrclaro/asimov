239aca0c9252ab73b5b74cb2af832c6aa1294462
John cClaro <jkrclaro@gmail.com>
2019-06-23 23:53:29 +0100

    Fix tests

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,26 +1,8 @@
-import pytest
-
 from src import token
 
 
 def test_should_pass_when_encrypted_token_is_valid():
-    data = {'email': 'john@doe.com'}
-    encrypted_token = token.encrypt(data)
-    decrypted_data = token.decrypt(encrypted_token)
-    assert decrypted_data.get('email') == 'john@doe.com'
-
-
-def test_should_fail_when_encrypted_token_is_expired():
-    data = {'email': 'john@doe.com'}
-    encrypted_token = token.encrypt(data)
-
-    with pytest.raises(token.SignatureExpired):
-        token.decrypt(encrypted_token, max_age=-1)
-
-
-def test_should_pass_when_encrypted_token_is_decrypted():
-    data = {'email': 'john@doe.com'}
-    encrypted_token = token.encrypt(data)
-
-    decrypted_data = token.decrypt(encrypted_token)
-    assert data.get('email') == decrypted_data.get('email')
+    email = 'john@doe.com'
+    token_encrypted = token.encrypt(email)
+    token_decrypted = token.decrypt(token_encrypted)
+    assert token_decrypted == email
