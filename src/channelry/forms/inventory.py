a00946e394557c936afb2c61ffc217d1fdcda7b2
John cClaro <jkrclaro@gmail.com>
2019-06-23 23:43:51 +0100

    Use different font weight for Open Sans

--- src/channelry/forms/inventory.py
+++ src/channelry/forms/inventory.py
@@ -1,39 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField
 from wtforms.widgets import TextArea
-from wtforms.validators import (
-    Email,
-    InputRequired,
-    EqualTo,
-    Length
-)
-from wtforms.fields.html5 import EmailField
 
 
 class InventoryCreateForm(FlaskForm):
-    name = StringField('Name:', render_kw={'placeholder': 'Blue t-shirt'})
+    name = StringField('Name', render_kw={'placeholder': 'Blue t-shirt'})
     caption = StringField(
-        'Caption:',
+        'Caption',
         description='A short one-line description of the product.',
         render_kw={'placeholder': 'Comfortable blue cotton t-shirt'}
     )
-    description = StringField('Description:', widget=TextArea())
+    description = StringField('Description', widget=TextArea())
     url = StringField(
-        'URL:',
+        'URL',
         description='The URL of the webpage for the product.',
         render_kw={'placeholder': 'example.com/blue-shirt'}
     )
     attributes = StringField(
-        'Attributes:',
+        'Attributes',
         description='A comma-separated list of attributes that define the '
                     'SKUs for this product (e.g. color, size, gender).',
         render_kw={'placeholder': 'size, style, color'}
     )
     active = BooleanField(
-        'Active:',
+        'Active',
         description='This item is available for purchase.'
     )
     product_id = IntegerField(
-        'ID:',
+        'ID',
         description="If an ID isn't provided, we'll generate one for you."
     )
