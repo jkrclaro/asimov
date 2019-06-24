eafef55fbc2bf95a867d85f2b513e4907d8924dd
John cClaro <jkrclaro@gmail.com>
2019-06-24 05:37:17 +0100

    Working edit settings

--- src/channelry/views/profile.py
+++ src/channelry/views/profile.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, current_app
 from flask_login import login_required, current_user
 
 from src.channelry.models import db
@@ -16,7 +16,33 @@ def index():
     change_profile_form = ChangeProfileForm()
     change_password_form = ChangePasswordForm()
 
-    if change_profile_form.validate_on_submit():
+    old_password = change_password_form.old_password.data
+    new_password = change_password_form.new_password.data
+    passwords_provided = (old_password and new_password)
+    if passwords_provided and change_password_form.validate_on_submit():
+        if old_password == new_password:
+            flash(
+                'New password should be different from old password',
+                'danger'
+            )
+        elif current_user.password_match(old_password):
+            edited = True
+            hashed_password = current_user.password_hash(new_password)
+            current_user.password = hashed_password.decode('utf8')
+            email = current_user.email
+            name = current_user.name
+            endpoint = 'auth.reset'
+            email_template = 'email/change_password_success.html'
+            subject = 'Your Stripe password has been changed'
+            context = {
+                'endpoint': endpoint,
+                'name': name
+            }
+            send_email(email, email_template, subject, **context)
+            flash('Successfully changed your Channelry password', 'success')
+        else:
+            flash('Wrong old password.', 'danger')
+    elif change_profile_form.validate_on_submit():
         edited = False
         if change_profile_form.email.data != current_user.email:
             email = change_profile_form.email.data
@@ -32,33 +58,15 @@ def index():
             flash(f"To finish changing your email address, we've sent an email to {email}. Simply click the link in the email to complete the process.", 'success')
         elif change_profile_form.name.data != current_user.name:
             current_user.name = change_profile_form.name.data
-            flash('Updated name.', 'success')
+            flash('Successfully changed your Channelry name.', 'success')
             edited = True
-
-    if change_password_form.validate_on_submit():
-        current_user.logger.debug(current_user.password_match(change_password_form.old_password.data))
-        if current_user.password_match(change_password_form.old_password.data):
-            change_password_form.old_password.errors = [
-                'New password should be different from old password'
-            ]
-        else:
-            edited = True
-            new_password = change_password_form.new_password.data
-            current_user.password = current_user.password_hash(new_password)
-            email = current_user.email
-            name = current_user.name
-            endpoint = 'auth.reset'
-            email_template = 'email/change_password_success.html'
-            subject = 'Your Stripe password has been changed'
-            send_email(email, email_template, subject, endpoint=endpoint, name=name)
-
-    else:
-        change_profile_form.email.data = current_user.email
-        change_profile_form.name.data = current_user.name
 
     if edited:
         db.session.add(current_user)
         db.session.commit()
+
+    change_profile_form.email.data = current_user.email
+    change_profile_form.name.data = current_user.name
 
     forms = {
         'change_profile_form': change_profile_form,
