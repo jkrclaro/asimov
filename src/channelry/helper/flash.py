f37f440e7bb839212cb3d1a24704e1c8813f9eb0
John cClaro <jkrclaro@gmail.com>
2019-06-25 11:35:27 +0100

    Documentation

--- src/channelry/helper/flash.py
+++ src/channelry/helper/flash.py
@@ -4,7 +4,7 @@ from flask import flash
 def flash_errors(errors: list) -> None:
     """Flashes error message then removes it from the list.
 
-    :param errors:
+    :param errors: Flask-WTF form errors that needs to be removed once flashed
     :return: None
     """
     for index, error in enumerate(errors[:]):
