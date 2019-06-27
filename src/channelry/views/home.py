d8523ef96b99f77810cd1334d1f93512c781a5bf
John cClaro <jkrclaro@gmail.com>
2019-06-27 17:23:35 +0100

    Proper layout for 500 error page

--- src/channelry/views/home.py
+++ src/channelry/views/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, current_app, redirect, url_for
+from flask import Blueprint, render_template, current_app, redirect, url_for, abort
 from flask_login import current_user
 
 
