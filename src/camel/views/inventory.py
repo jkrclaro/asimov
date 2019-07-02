473840570c1c073df3b3586062a04a2f84db8b52
John cClaro <jkrclaro@gmail.com>
2019-07-02 14:09:06 +0100

    Tidy ups

--- src/camel/views/inventory.py
+++ src/camel/views/inventory.py
@@ -4,10 +4,10 @@ from flask_login import login_required, current_user
 from src.camel.models.dashboard import Product
 
 
-inventory_bp = Blueprint('inventory', __name__)
+inventory_bp = Blueprint('inventory', __name__, url_prefix='/inventory')
 
 
-@inventory_bp.route('/inventories')
+@inventory_bp.route('/')
 @login_required
 def index():
     products = Product.query.filter_by(account_id=current_user.account.id)
