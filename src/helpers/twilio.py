a987d99e735b213685961b63fec1c1eb600eb67e
John Claro <jkrclaro@gmail.com>
2019-12-08 19:16:31 +0000

    Add helper to have a factory method approach for Twilio

--- src/helpers/twilio.py
+++ src/helpers/twilio.py
@@ -1,4 +1,41 @@
+from flask import current_app
+from flask import _app_ctx_stack as stack
+
 from twilio.rest import Client
+
+
+class Twilio:
+
+    def __init__(self, app=None):
+        self.app = app
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.teardown_appcontext(self.teardown)
+
+    @property
+    def client(self):
+        ctx = stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'twilio_client'):
+                username = current_app.config.get('TWILIO_AUTH_SID')
+                account_sid = current_app.config.get('TWILIO_ACCOUNT_SID')
+                if username is None:
+                    username = account_sid
+                elif account_sid is None:
+                    account_sid = username
+                password = current_app.config['TWILIO_AUTH_TOKEN']
+                ctx.twilio_client = Client(
+                    username=username, password=password,
+                    account_sid=account_sid
+                )
+            return ctx.twilio_client
+
+    def teardown(self, exception):
+        context = stack.top
+        if hasattr(context, 'twilio_client'):
+            del context.twilio_client
 
 
 def get_phones(client: Client) -> list:
--- src/server/views/sms.py
+++ src/server/views/sms.py
@@ -6,42 +6,18 @@ from flask import (
     g
 )
 
-from twilio.rest import Client
 from twilio.base.exceptions import TwilioException, TwilioRestException
 
 from ..forms import SMSForm
+from src.server import twilio
 
 
 sms_bp = Blueprint('sms', __name__, url_prefix='/sms')
 
 
-@sms_bp.route('/')
-def index():
-    account_sid = current_app.config['TWILIO_ACCOUNT_SID']
-    auth_token = current_app.config['TWILIO_AUTH_TOKEN']
-    client = Client(account_sid, auth_token)
-
-    messages = client.messages.list()
-    chats = {}
-
-    for message in messages:
-        number = message.to
-        if number not in chats.keys():
-            chats[number] = {
-                'last_message': message.body,
-                'last_message_date_sent': message.date_sent,
-            }
-
-    return render_template('sms/index.html', chats=chats)
-
-
 @sms_bp.route('/<number>', methods=['GET'])
 def get_chats(number):
-    account_sid = current_app.config['TWILIO_ACCOUNT_SID']
-    auth_token = current_app.config['TWILIO_AUTH_TOKEN']
-    client = Client(account_sid, auth_token)
-
-    chats = client.messages.list(to=number)
+    chats = twilio.client.messages.list(to=number)
 
     form = SMSForm()
 
@@ -51,7 +27,9 @@ def get_chats(number):
         message = form.message.data
 
         try:
-            client.messages.create(body=message, from_=sender, to=receiver)
+            current_app.twilio.messages.create(
+                body=message, from_=sender, to=receiver
+            )
             message, category = f"Successfully sent your " \
                                 f"message to {receiver}", 'success'
         except (TwilioException, TwilioRestException) as error:
@@ -66,20 +44,30 @@ def create():
     form = SMSForm()
 
     if form.validate_on_submit():
-        account_sid = current_app.config['TWILIO_ACCOUNT_SID']
-        auth_token = current_app.config['TWILIO_AUTH_TOKEN']
-        client = Client(account_sid, auth_token)
-
-        sender = g.current_phone['number']
-        receiver = form.receiver.data
+        sender = form.sender.data
+        recipient = form.receiver.data
         message = form.message.data
+        platform = form.platform.data
+        message_success = f"Successfully sent your message to {recipient}"
 
-        try:
-            client.messages.create(body=message, from_=sender, to=receiver)
-            message, category = f"Successfully sent your " \
-                                f"message to {receiver}", 'success'
-        except (TwilioException, TwilioRestException) as error:
-            message, category = error.msg, 'danger'
+        if platform == 'telnyx':
+            message = 'Successfully sent message using Telnyx'
+            category = 'success'
+        elif platform == 'twilio':
+            try:
+                data = {
+                    'from_': sender,
+                    'to': recipient,
+                    'body': message
+                }
+                current_app.twilio.create(**data)
+                message, category = message_success, 'success'
+            except (TwilioException, TwilioRestException) as error:
+                message, category = error.msg, 'danger'
+        else:
+            message = 'Unknown communications platform. Message not sent.'
+            category = 'danger'
+
         flash(message, category)
 
     return render_template('sms/create.html', form=form)
