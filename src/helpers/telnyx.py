20487bc2dd1d3b4b9f95c0523c24548996cc401a
John cClaro <jkrclaro@gmail.com>
2019-12-09 19:22:13 +0000

    Catch InvalidRequestError and AuthenticationError

--- src/helpers/telnyx.py
+++ src/helpers/telnyx.py
@@ -13,7 +13,12 @@ def get_phones(client: telnyx) -> list:
     ]
 
 
-def sms_build(payload):
+def sms_build(payload: dict) -> dict:
+    """Build SMS payload to be used for sending.
+
+    :param payload:
+    :return:
+    """
     return {
         'from': payload['sender'],
         'to': payload['receiver'],
@@ -22,7 +27,7 @@ def sms_build(payload):
 
 
 def sms_send(client: telnyx, sms: dict) -> (str, str):
-    """
+    """Send an SMS message.
 
     :param client:
     :param sms:
@@ -38,8 +43,14 @@ def sms_send(client: telnyx, sms: dict) -> (str, str):
         else:
             message = '???'
     except telnyx.error.PermissionError as permission_error:
-        message = permission_error.errors[0]['detail']
+        detail = permission_error.errors[0]['detail']
+        meta = permission_error.errors[0]['meta']['url']
+        message = f'{detail} {meta}'
     except telnyx.error.APIConnectionError as api_connection_error:
         message = api_connection_error.user_message
+    except telnyx.error.InvalidRequestError as invalid_request_error:
+        message = invalid_request_error
+    except telnyx.error.AuthenticationError as authentication_error:
+        message = authentication_error
 
     return message, category
