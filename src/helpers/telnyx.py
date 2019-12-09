2e0c70515433848e289ca673d3ffe868a3950ecd
John Claro <jkrclaro@gmail.com>
2019-12-09 17:18:55 +0000

    Add helpers.telnyx.sms_create

--- src/helpers/telnyx.py
+++ src/helpers/telnyx.py
@@ -11,3 +11,28 @@ def get_phones(client: telnyx) -> list:
         {'number': phone['phone_number'], 'platform': 'telnyx'}
         for phone in client.MessagingPhoneNumber.list()['data']
     ]
+
+
+def sms_create(client: telnyx, message: dict) -> (str, str):
+    """
+
+    :param client:
+    :param message:
+    :return:
+    """
+    response = {}
+    category = 'danger'
+
+    try:
+        response = client.Message.create(**message)
+        if response:
+            message = 'Message sent via Telnyx'
+            category = 'success'
+        else:
+            message = '???'
+    except telnyx.error.PermissionError as permission_error:
+        message = permission_error.errors[0]['detail']
+    except telnyx.error.APIConnectionError as api_connection_error:
+        message = api_connection_error.user_message
+
+    return message, category
