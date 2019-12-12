625258b550de774ed648a7737df307b2a2fef081
John cClaro <jkrclaro@gmail.com>
2019-12-12 15:50:12 +0000

    Initial commit

--- app.py
+++ app.py
@@ -2,15 +2,7 @@ import os
 import logging
 from dotenv import find_dotenv, load_dotenv
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-from src.warkphone import create_app
-=======
-from src.sidefone import create_app
->>>>>>> Add speech-bubble CSS
-=======
 from src.server import create_app
->>>>>>> Rename as server
 
 logging.getLogger().setLevel(logging.INFO)
 load_dotenv(find_dotenv('env/development.env'))
