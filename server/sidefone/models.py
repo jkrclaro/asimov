4ee782c58ef9138926072463ed3eb1329277f31a
John Claro <jkrclaro@gmail.com>
2020-01-06 17:37:06 +0000

    Add Contact model

--- server/sidefone/models.py
+++ server/sidefone/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=254)
+    phone = PhoneNumberField()
+    job = models.CharField(max_length=254)
+    website = models.URLField(max_length=200)
+    address = models.CharField(max_length=254)
+    birthday = models.DateField()
+    company = models.CharField(max_length=254)
+    agent = models.CharField(max_length=254)
+    background = models.TextField()
+
+    class Meta:
+        db_table = 'sidefone_contacts'
