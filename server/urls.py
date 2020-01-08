650853bbb996f5dd881242faec7c2ef3d33bcecd
John Claro <jkrclaro@gmail.com>
2020-01-08 12:20:53 +0000

    Add api prefix

--- server/urls.py
+++ server/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('articles/', TemplateView.as_view(template_name='articles.html'), name='home'),
     path('api/', include(router.urls)),
-    path('sidefone/', include('server.sidefone.urls')),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/sidefone/', include('server.sidefone.urls')),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
