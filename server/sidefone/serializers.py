f7bbad3087c11d7826544b9844fa2ba151b51bbe
John cClaro <jkrclaro@gmail.com>
2020-01-08 13:16:31 +0000

    Publicise all fields for Contact model

--- server/sidefone/serializers.py
+++ server/sidefone/serializers.py
@@ -6,7 +6,4 @@ from .models import Contact
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Contact
-        fields = [
-            'email', 'phone', 'job', 'website', 'address', 'birthday',
-            'company', 'agent', 'background'
-        ]
+        fields = '__all__'
