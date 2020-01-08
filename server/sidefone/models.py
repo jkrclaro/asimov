67e948e30fcd1c5a7d85b87b8fcb133f1ff51429
John cClaro <jkrclaro@gmail.com>
2020-01-08 16:27:59 +0000

    Set all Contact fields to be nullable except name

--- server/sidefone/models.py
+++ server/sidefone/models.py
@@ -5,16 +5,16 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    phone = PhoneNumberField()
-    job = models.CharField(max_length=254)
-    website = models.URLField(max_length=200)
-    address = models.CharField(max_length=254)
-    birthday = models.DateField()
-    company = models.CharField(max_length=254)
-    agent = models.CharField(max_length=254)
-    background = models.TextField()
-    img = models.ImageField(upload_to='sidefone')
+    email = models.EmailField(max_length=254, null=True, blank=True)
+    phone = PhoneNumberField(null=True, blank=True)
+    job = models.CharField(max_length=254, null=True, blank=True)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=254, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    company = models.CharField(max_length=254, null=True, blank=True)
+    agent = models.CharField(max_length=254, null=True, blank=True)
+    background = models.TextField(null=True, blank=True)
+    img = models.ImageField(upload_to='sidefone', null=True, blank=True)
 
     class Meta:
         db_table = 'sidefone_contacts'
