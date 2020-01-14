fd1e2064cb975f79b149e8a7bbd998b750625b8b
John Claro <jkrclaro@gmail.com>
2020-01-14 13:58:01 +0000

    Update storage.py

--- server/storage.py
+++ server/storage.py
@@ -5,9 +5,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class Static(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    bucket_name = f"assets.{settings.AWS_STORAGE_BUCKET_NAME}"
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
 
 class Media(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    custom_domain = settings.AWS_S3_MEDIA_DOMAIN
