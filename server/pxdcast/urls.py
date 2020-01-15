7184524c824224f08ebb86355eaf44bb5811d3df
John Claro <jkrclaro@gmail.com>
2020-01-15 20:42:26 +0000

    Add podcast_retrieve and a 1 second delay

--- server/pxdcast/urls.py
+++ server/pxdcast/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import podcast_list
+from .views import podcast_list, podcast_retrieve
 
 app_name = 'pxdcast'
 
 urlpatterns = [
-    path('podcasts', podcast_list, name='podcast_list')
+    path('podcasts', podcast_list, name='podcast_list'),
+    re_path(r'podcasts/(?P<pk>\d+)/$', podcast_retrieve, name='podcast_retrieve')
 ]
