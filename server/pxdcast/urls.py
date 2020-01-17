cc8288cb11eec0a1d50039f4b73ae0fa329a211c
John Claro <jkrclaro@gmail.com>
2020-01-17 02:05:09 +0000

    Add episode_list and episode_retrieve URLs

--- server/pxdcast/urls.py
+++ server/pxdcast/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path
 
-from .views import podcast_list, podcast_retrieve
+from .views import podcast_list, podcast_retrieve, episode_list, episode_retrieve
 
 app_name = 'pxdcast'
 
 urlpatterns = [
     path('podcasts', podcast_list, name='podcast_list'),
-    re_path(r'podcasts/(?P<pk>\d+)/$', podcast_retrieve, name='podcast_retrieve')
+    re_path(r'podcasts/(?P<pk>\d+)/$', podcast_retrieve, name='podcast_retrieve'),
+    re_path(r'podcasts/(?P<pk>\d+)/episodes/$', episode_list, name='episode_list'),
+    re_path(r'podcasts/(?P<podcast_pk>\d+)/episodes/(?P<pk>\d+)/$', episode_retrieve, name='episode_retrieve'),
 ]
