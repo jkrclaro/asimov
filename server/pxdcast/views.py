797714f00dfad72c0552d1fc598fe001b4047a1c
John cClaro <jkrclaro@gmail.com>
2020-01-17 02:05:09 +0000

    Add episode_list and episode_retrieve URLs

--- server/pxdcast/views.py
+++ server/pxdcast/views.py
@@ -28,26 +28,42 @@ def podcast_retrieve(request, pk):
         'img': 'https://storage.googleapis.com/indie-hackers.appspot.com/product-avatars/software-engineering-daily/PWairgiOpneHvkGJri7RVbtORKI2?',
         'author': 'Jeff Meyerson',
         'website': 'softwareengineeringdaily.com',
-        'summary': "Technical interviews about software topics.",
-        'episodes': [
-            {
-                'id': 3,
-                'name': 'JS Party with Kevin Ball',
-                'uploaded_at': 'January 16',
-                'duration': '1h 4m'
-            },
-            {
-                'id': 2,
-                'name': 'Packet: Baremetal Infrastructure with Zachary Smith and Nathan Goulding',
-                'uploaded_at': 'January 15',
-                'duration': '53 mins'
-            },
-            {
-                'id': 1,
-                'name': 'Edge Computing Platform with Jaromir Coufal',
-                'uploaded_at': 'January 14',
-                'duration': '53 mins'
-            },
-        ]
+        'summary': "Technical interviews about software topics."
+    }
+    return jsonify(data)
+
+
+def episode_list(request, pk):
+    time.sleep(1)
+    data = [
+        {
+            'id': 'js-party-with-kevin-ball',
+            'name': 'JS Party with Kevin Ball',
+            'uploaded_at': 'January 16',
+            'duration': '1h 4m'
+        },
+        {
+            'id': 'packet-baremetal-infrastructure-with-zachary-smith-and-nathan-goulding',
+            'name': 'Packet: Baremetal Infrastructure with Zachary Smith and Nathan Goulding',
+            'uploaded_at': 'January 15',
+            'duration': '53 mins'
+        },
+        {
+            'id': 'edge-computing-platform-with-jaromir-coufal',
+            'name': 'Edge Computing Platform with Jaromir Coufal',
+            'uploaded_at': 'January 14',
+            'duration': '53 mins'
+        },
+    ]
+    return jsonify(data)
+
+
+def episode_retrieve(request, podcast_pk, pk):
+    time.sleep(1)
+    data = {
+        'id': 'js-party-with-kevin-ball',
+        'name': 'JS Party with Kevin Ball',
+        'uploaded_at': 'January 16',
+        'duration': '1h 4m'
     }
     return jsonify(data)
