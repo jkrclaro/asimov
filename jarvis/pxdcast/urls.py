34575ac454b040a5801a90689c37171d9b475b11
John Claro <jkrclaro@gmail.com>
2020-01-23 17:10:35 +0000

    Add subscriptions URL

--- jarvis/pxdcast/urls.py
+++ jarvis/pxdcast/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, re_path
 
-from .views import podcast_list, podcast_retrieve, episode_list, episode_retrieve
+from .views import (
+    podcast_list, podcast_retrieve, episode_list, episode_retrieve,
+    podcast_subscriptions
+)
 
 app_name = 'pxdcast'
 
 urlpatterns = [
+    path('subscriptions', podcast_subscriptions, name='podcast_subscriptions'),
     path('podcasts', podcast_list, name='podcast_list'),
     re_path(r'podcasts/(?P<pk>\d+)/$', podcast_retrieve, name='podcast_retrieve'),
     re_path(r'podcasts/(?P<pk>\d+)/episodes/$', episode_list, name='episode_list'),
