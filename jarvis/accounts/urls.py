eda7c82feb00bd9d585ac4d3d6f1241491886d82
John cClaro <jkrclaro@gmail.com>
2020-01-30 11:29:07 +0000

    Implement simplejwt

--- jarvis/accounts/urls.py
+++ jarvis/accounts/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 
-from knox.views import LogoutView
-
-from .views import UserAPIView, RegisterAPIView, LoginAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import register
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include('knox.urls')),
-    path('user', UserAPIView.as_view(), name='user'),
-    path('register', RegisterAPIView.as_view(), name='register'),
-    path('login', LoginAPIView.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='knox_logout')
+    path('register', register, name='register'),
+    path('login', TokenObtainPairView.as_view(), name='login'),
+    path('refresh', TokenRefreshView.as_view(), name='refresh')
 ]
