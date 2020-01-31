ca0e4e1aa291b57d6a045b8c60dbbf6fa971bc84
John cClaro <jkrclaro@gmail.com>
2020-01-31 13:02:59 +0000

    Add search fields for Podcast model

--- jarvis/pxdcast/admin.py
+++ jarvis/pxdcast/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Podcast, Episode, Subscription
 
-admin.site.register(Podcast)
+
+class PodcastAdmin(admin.ModelAdmin):
+    search_fields = ('apple_podcasts_id', 'name')
+
+
+admin.site.register(Podcast, PodcastAdmin)
 admin.site.register(Episode)
 admin.site.register(Subscription)
