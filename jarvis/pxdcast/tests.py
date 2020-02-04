7c9e3d2382cf3363dc0fa52f2efd5c5437dcd50c
John cClaro <jkrclaro@gmail.com>
2020-02-04 15:16:15 +0000

    Handle format of duration when total seconds

--- jarvis/pxdcast/tests.py
+++ jarvis/pxdcast/tests.py
@@ -9,6 +9,8 @@ class FeedTestCase(TestCase):
         self.assertEqual(feed.format_duration('01:36:41'), '1h 36m')
         self.assertEqual(feed.format_duration('00:36:41'), '36 mins')
         self.assertEqual(feed.format_duration('00:00:41'), '41 secs')
+        self.assertEqual(feed.format_duration('0:40:12'), '40 mins')
+        self.assertEqual(feed.format_duration('2412'), '40 mins')
 
     def test_format_uploaded_at(self):
         self.assertEqual(1, 1)
