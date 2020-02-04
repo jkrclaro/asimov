b92a90a26d6c4735a0ada865ffbe006c5a9eb2de
John Claro <jkrclaro@gmail.com>
2020-02-04 15:46:29 +0000

    Return datetimefield format for published

--- jarvis/pxdcast/tests.py
+++ jarvis/pxdcast/tests.py
@@ -13,4 +13,4 @@ class FeedTestCase(TestCase):
         self.assertEqual(feed.format_duration('2412'), '40 mins')
 
     def test_format_uploaded_at(self):
-        self.assertEqual(feed.format_published_at('Sun, 25 Aug 2019 11:00:00 +0000'), 'Aug 25, 2019')
+        self.assertEqual(feed.format_published_at('Sun, 25 Aug 2019 11:00:00 +0000'), '2019-08-25 11:00')
