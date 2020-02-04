39b184976120f8810940d251236d259058d585a6
John cClaro <jkrclaro@gmail.com>
2020-02-04 14:50:51 +0000

    Make duration formats verbose

--- jarvis/pxdcast/tests.py
+++ jarvis/pxdcast/tests.py
@@ -7,5 +7,8 @@ class FeedTestCase(TestCase):
 
     def test_format_duration(self):
         self.assertEqual(feed.format_duration('01:36:41'), '1h 36m')
-        self.assertEqual(feed.format_duration('00:36:41'), '36m 41s')
-        self.assertEqual(feed.format_duration('00:00:41'), '41s')
+        self.assertEqual(feed.format_duration('00:36:41'), '36 mins')
+        self.assertEqual(feed.format_duration('00:00:41'), '41 secs')
+
+    def test_format_uploaded_at(self):
+        self.assertEqual(1, 1)
