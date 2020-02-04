4bc48ce57328ce9304f8b4b47d0b207080085fc9
John cClaro <jkrclaro@gmail.com>
2020-02-04 18:50:31 +0000

    Check if duration has colons

--- jarvis/pxdcast/tests.py
+++ jarvis/pxdcast/tests.py
@@ -11,6 +11,7 @@ class FeedTestCase(TestCase):
         self.assertEqual(feed.format_duration('00:00:41'), '41 secs')
         self.assertEqual(feed.format_duration('0:40:12'), '40 mins')
         self.assertEqual(feed.format_duration('2412'), '40 mins')
+        self.assertEqual(feed.format_duration('58:55'), '58 mins')
 
     def test_format_uploaded_at(self):
         self.assertEqual(feed.format_published_at('Sun, 25 Aug 2019 11:00:00 +0000'), '2019-08-25 11:00')
