5c39c241383d405f846bd10142d8ed91062b28d0
John Claro <jkrclaro@gmail.com>
2020-02-04 14:48:31 +0000

    Formatting of duration

--- jarvis/pxdcast/tests.py
+++ jarvis/pxdcast/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 
+from .helpers import feed
+
 
 class FeedTestCase(TestCase):
 
     def test_format_duration(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(feed.format_duration('01:36:41'), '1h 36m')
+        self.assertEqual(feed.format_duration('00:36:41'), '36m 41s')
+        self.assertEqual(feed.format_duration('00:00:41'), '41s')
