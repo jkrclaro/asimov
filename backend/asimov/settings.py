dda58f7303c34351e6a950b11fc3ffeb528347ae
John cClaro <jkrclaro@gmail.com>
2020-02-08 22:39:23 +0000

    Use Gunicorn

--- backend/asimov/settings.py
+++ backend/asimov/settings.py
@@ -43,7 +43,7 @@ MESSAGE_TAGS = {
 }
 
 if DEBUG:
-    ALLOWED_HOSTS = ('asimov.localhost',)
+    ALLOWED_HOSTS = ['*']
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
     CORS_ORIGIN_ALLOW_ALL = True
