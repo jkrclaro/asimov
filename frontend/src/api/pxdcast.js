b205926cad6719acfdd3f8e300b63a735d709000
John cClaro <jkrclaro@gmail.com>
2020-02-08 20:47:40 +0000

    Rename jarvis to asimov

--- frontend/src/api/pxdcast.js
+++ frontend/src/api/pxdcast.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://jarvis.jkrclaro.com/pxdcast' : 'http://jarvis.localhost:8000/pxdcast'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://asimov.jkrclaro.com/pxdcast' : 'http://asimov.localhost:8000/pxdcast'
 })
