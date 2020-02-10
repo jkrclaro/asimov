c757d19c797994c8df7143596cd9e74681f7a580
John Claro <jkrclaro@gmail.com>
2020-02-10 02:04:58 +0000

    Adjust container on Layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,21 +6,23 @@ class Layout extends React.Component {
 
     render() {
         return (
-            <div className='container'>
+            <div>
                 <nav className='navbar navbar-expand-lg navbar-light mt-3 mb-3' id='main-navbar'>
-                    <Link to='/' className='mr-3'><img src='/logo.png' alt='logo.png' height='30' width='30'></img></Link>
-                    <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbar-content' aria-controls='navbar-content' aria-expanded='false' aria-label='Toggle navigation'>
-                        <span className='navbar-toggler-icon'></span>
-                    </button>
+                    <div className='container'>
+                        <Link to='/' className='mr-3'><img src='/logo.png' alt='logo.png' height='30' width='30'></img></Link>
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbar-content' aria-controls='navbar-content' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
 
-                    <div className='collapse navbar-collapse' id='navbar-content'>
-                        <div className='navbar-nav ml-auto mt-lg-0'>
-                            <Link to='/login' className='nav-item nav-link mr-3'>Login</Link>
-                            <Link to='/register' className='nav-item nav-link'>Sign Up</Link>
+                        <div className='collapse navbar-collapse' id='navbar-content'>
+                            <div className='navbar-nav ml-auto mt-lg-0'>
+                                <Link to='/login' className='nav-item nav-link mr-3'>Login</Link>
+                                <Link to='/register' className='nav-item nav-link'>Sign Up</Link>
+                            </div>
                         </div>
                     </div>
                 </nav>
-                <div className='mt-3 mb-5'>
+                <div className='container mt-3 mb-5'>
                     {this.props.children}
                 </div>
             </div>
