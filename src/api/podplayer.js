750b8c353044a804197f4a4845e08f09f7e55234
John Claro <jkrclaro@gmail.com>
2021-03-11 22:28:50 +0000

    Update URLs

--- src/api/podplayer.js
+++ src/api/podplayer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://podplayer.johnclaro.com/' : 'http://podplayer.localhost:8000/'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://johnclaro.com/podplayer' : 'http://localhost:8000/podplayer'
 })
