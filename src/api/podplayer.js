0b4fac818a33946f82c1a5f79c3531ef9a981426
John Claro <jkrclaro@gmail.com>
2021-03-11 22:52:21 +0000

    Prepend www

--- src/api/podplayer.js
+++ src/api/podplayer.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'https://johnclaro.com/podplayer' : 'http://localhost:8000/podplayer'
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://www.johnclaro.com/podplayer' : 'http://localhost:8000/podplayer'
 })
