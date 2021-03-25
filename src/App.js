39bc359ae01419a8300062c2a02249bde64633b3
John Claro <jkrclaro@gmail.com>
2021-03-25 16:38:03 +0000

    Remove auth flow

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import { loadUser } from './actions/auth';
+import { loadUser, login } from './actions/auth';
 import PodcastsPage from './components/PodcastsPage';
 import SearchRedirector from './components/SearchRedirector';
 import PodcastPage from './components/PodcastPage';
 import NotFoundPage from './components/NotFoundPage';
-import PublicRoute from './helpers/PublicRoute';
 import Player from './containers/Player';
 
 
@@ -17,6 +16,8 @@ class App extends React.Component {
     componentDidMount() {
         if (localStorage.getItem('token')) {
             store.dispatch(loadUser());
+        } else {
+            store.dispatch(login({ username: 'podplayer', password: 'p0dpl4y3r!p455'}))
         }
     }
 
@@ -25,7 +26,7 @@ class App extends React.Component {
             <Provider store={store}>
                 <BrowserRouter basename={process.env.PUBLIC_URL}>
                     <Switch>
-                        <PublicRoute exact path={process.env.PUBLIC_URL + '/'} component={PodcastsPage} />
+                        <Route exact path={process.env.PUBLIC_URL + '/'} component={PodcastsPage} />
                         <Route exact path={process.env.PUBLIC_URL + '/search/:id'} component={SearchRedirector} />
                         <Route exact path={process.env.PUBLIC_URL + '/podcasts/:id'} component={PodcastPage} />
                         <Route component={NotFoundPage} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,12 @@ class Header extends React.Component {
 
     render() {
         let { path, title } = this.props;
-        path = path ? path.replace('/:id', '') : path;
-
         return (
             <div>
                 <div className='row mt-3 mb-3'>
                     <div className='col-lg-7 my-auto'>
                             { path ? (
-                                <Link className='search-link' to={path}><i className='fas fa-arrow-left mr-2'></i> Back</Link>
+                                <Link className='search-link' to='/'><i className='fas fa-arrow-left mr-2'></i> Back</Link>
                             ) : (
                                 <h3><b>{title}</b></h3>
                             )}
